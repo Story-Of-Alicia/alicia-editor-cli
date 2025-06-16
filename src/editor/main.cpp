eb5effc32ba08f859a9193d7d1c010de4588210d
rgnter <maros.prejsa1@gmail.com>
2025-06-16 17:03:52 +0200

    init

---
+++ src/editor/main.cpp
@@ -0,0 +1,93 @@
+#include <libpak/libpak.hpp>
+
+#include <cstdio>
+#include <ranges>
+#include <filesystem>
+#include <fstream>
+
+namespace
+{
+
+void export_assets(libpak::resource& pak)
+{
+  for (const auto& asset : pak.assets | std::views::values)
+  {
+    if (asset.data.buffer.empty())
+      continue;
+
+    std::filesystem::path asset_path(asset.header.path);
+    std::filesystem::create_directories(
+      asset_path.parent_path());
+
+    std::ofstream file(asset_path, std::ios::binary);
+    if (!file.is_open())
+    {
+      printf("Couldn't write\n");
+      return;
+    }
+
+    file.write(
+      reinterpret_cast<const char*>(asset.data.buffer.data()),
+      static_cast<int64_t>(asset.data.buffer.size()));
+  }
+}
+
+void import_assets(libpak::resource& pak)
+{
+  for (auto& asset : pak.assets | std::views::values)
+  {
+    if (not asset.header.is_asset_embedded)
+      continue;
+
+    std::filesystem::path asset_path(asset.header.path);
+    std::ifstream file(asset_path, std::ios::binary);
+    if (!file.is_open())
+    {
+      continue;
+    }
+
+    wprintf(L"Patching '%ls'\n", asset.header.path);
+
+    file.seekg(0, std::ios::end);
+    const auto size = file.tellg();
+    file.seekg(0);
+
+    asset.data.buffer.resize(size);
+    file.read(reinterpret_cast<char*>(asset.data.buffer.data()), size);
+
+    asset.header.data_decompressed_length = size;
+    asset.header.data_decompressed_length0 = size;
+    asset.header.data_decompressed_length1 = size;
+  }
+}
+
+void set_all_dev(libpak::resource& pak)
+{
+  for (auto& asset : pak.assets | std::views::values)
+  {
+    asset.header.is_asset_embedded = false;
+    asset.header.embedded_data_length = 0;
+    asset.header.embedded_data_offset = 0;
+    asset.header.crc_embedded = 0x0;
+    asset.header.checksum_embedded = 0x0;
+  }
+}
+
+} // anon namespace
+
+int main(int argc, const char** args)
+{
+  libpak::resource pak("res.pak");
+  printf("Reading..\n");
+  pak.read(false);
+
+  printf("Importing..\n");
+  import_assets(pak);
+
+  printf("Writing..\n");
+  pak.resource_path = "res.pak.prod";
+  pak.write();
+
+  printf("Done.\n");
+  return 0;
+}
